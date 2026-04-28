3ff71c2fdd0bc7f2ef9016be3afc938f4b0e70be
Reddy0402 <reddysaiteja688@gmail.com>
2026-04-28 19:26:39 +0530

    a bit code is written

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,21 @@
-os.getenv("groq_api_key")
+import os
+
+
+new = os.getenv("groq_api_key")
+
+ModelName = "llama3-8b-8192"
+
+print(ModelName)
+
+
+Messages=[
+    {"role":"student", "content":"what do you need"}
+]
+
+
+response = model(
+    ModelName,
+    Messages
+)
+
+print(Messages)
